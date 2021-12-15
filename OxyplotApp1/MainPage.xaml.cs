7f0771943ee72b0bde34a3303638f6e28c934542
ToolmakerSteve <ToolmakerSteve@users.noreply.github.com>
2021-12-15 13:36:17 -0600

    initial commit

---
+++ OxyplotApp1/MainPage.xaml.cs
@@ -0,0 +1,121 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+
+namespace OxyplotApp1
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			BindingContext = new MainPageViewModel();
+		}
+
+		void Expander_Tapped(System.Object sender, System.EventArgs e)
+		{
+			if (expander != null) {
+				expander.IsExpanded = false;
+			}
+
+			expander = sender as Expander;
+
+			int id = Convert.ToInt32(expander.ClassId);
+
+			ReportsClass item = new ReportsClass(); //ttt newKidList[id];
+
+
+			Device.StartTimer(TimeSpan.FromSeconds(1), () => {
+
+				if (item.chart == null) {
+
+					PlotModel model = new PlotModel();
+
+					CategoryAxis xaxis = new CategoryAxis();
+					xaxis.Position = AxisPosition.Bottom;
+					xaxis.MajorGridlineStyle = LineStyle.None;
+					xaxis.MinorGridlineStyle = LineStyle.None;
+					xaxis.MinorTickSize = 0;
+					xaxis.MajorTickSize = 0;
+					xaxis.TextColor = OxyColors.Gray;
+					xaxis.FontSize = 10.0;
+					xaxis.Labels.Add("S");
+					xaxis.Labels.Add("M");
+					xaxis.Labels.Add("T");
+					xaxis.Labels.Add("W");
+					xaxis.Labels.Add("T");
+					xaxis.Labels.Add("F");
+					xaxis.Labels.Add("S");
+					xaxis.GapWidth = 10.0;
+					xaxis.IsPanEnabled = false;
+					xaxis.IsZoomEnabled = false;
+
+
+					LinearAxis yaxis = new LinearAxis();
+					yaxis.Position = AxisPosition.Left;
+					yaxis.MajorGridlineStyle = LineStyle.None;
+					xaxis.MinorGridlineStyle = LineStyle.None;
+					yaxis.MinorTickSize = 0;
+					yaxis.MajorTickSize = 0;
+					yaxis.TextColor = OxyColors.Gray;
+					yaxis.FontSize = 10.0;
+					yaxis.FontWeight = FontWeights.Bold;
+					yaxis.IsZoomEnabled = false;
+					yaxis.IsPanEnabled = false;
+
+
+					ColumnSeries s2 = new ColumnSeries();
+					s2.TextColor = OxyColors.White;
+
+					//s2.Items.Add(new ColumnItem {
+					//	Value = item.Sunday,
+					//	Color = OxyColor.Parse("#02cc9d")
+					//});
+					//s2.Items.Add(new ColumnItem {
+					//	Value = item.Monday,
+					//	Color = OxyColor.Parse("#02cc9d")
+					//});
+					//s2.Items.Add(new ColumnItem {
+					//	Value = item.Tuesday,
+					//	Color = OxyColor.Parse("#02cc9d")
+					//});
+					//s2.Items.Add(new ColumnItem {
+					//	Value = item.Wednesday,
+					//	Color = OxyColor.Parse("#02cc9d")
+					//});
+					//s2.Items.Add(new ColumnItem {
+					//	Value = item.Thursday,
+					//	Color = OxyColor.Parse("#02cc9d")
+
+					//});
+					//s2.Items.Add(new ColumnItem {
+					//	Value = item.Friday,
+					//	Color = OxyColor.Parse("#02cc9d")
+					//});
+					//s2.Items.Add(new ColumnItem {
+					//	Value = item.Saturday,
+					//	Color = OxyColor.Parse("#02cc9d")
+					//});
+
+					model.Axes.Add(xaxis);
+					model.Axes.Add(yaxis);
+					model.Series.Add(s2);
+					model.PlotAreaBorderColor = OxyColors.Transparent;
+
+					//ttt item.chart = model;
+				}
+
+				return false;
+			});
+
+		}
+	}
+}
---
+++ OxyplotApp1/ReportsClass.cs
@@ -0,0 +1,92 @@
+﻿using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OxyplotApp1
+{
+	public class ReportsClass
+	{
+		public PlotModel chart {
+			get {
+				PlotModel model = new PlotModel();
+
+				CategoryAxis xaxis = new CategoryAxis();
+				xaxis.Position = AxisPosition.Bottom;
+				xaxis.MajorGridlineStyle = LineStyle.None;
+				xaxis.MinorGridlineStyle = LineStyle.None;
+				xaxis.MinorTickSize = 0;
+				xaxis.MajorTickSize = 0;
+				xaxis.TextColor = OxyColors.Gray;
+				xaxis.FontSize = 10.0;
+				xaxis.Labels.Add("S");
+				xaxis.Labels.Add("M");
+				xaxis.Labels.Add("T");
+				xaxis.Labels.Add("W");
+				xaxis.Labels.Add("T");
+				xaxis.Labels.Add("F");
+				xaxis.Labels.Add("S");
+				xaxis.GapWidth = 10.0;
+				xaxis.IsPanEnabled = false;
+				xaxis.IsZoomEnabled = false;
+
+
+				LinearAxis yaxis = new LinearAxis();
+				yaxis.Position = AxisPosition.Left;
+				yaxis.MajorGridlineStyle = LineStyle.None;
+				xaxis.MinorGridlineStyle = LineStyle.None;
+				yaxis.MinorTickSize = 0;
+				yaxis.MajorTickSize = 0;
+				yaxis.TextColor = OxyColors.Gray;
+				yaxis.FontSize = 10.0;
+				yaxis.FontWeight = FontWeights.Bold;
+				yaxis.IsZoomEnabled = false;
+				yaxis.IsPanEnabled = false;
+
+
+				ColumnSeries s2 = new ColumnSeries();
+				s2.TextColor = OxyColors.White;
+
+				//s2.Items.Add(new ColumnItem {
+				//	Value = Sunday,
+				//	Color = OxyColor.Parse("#02cc9d")
+				//});
+				//s2.Items.Add(new ColumnItem {
+				//	Value = Monday,
+				//	Color = OxyColor.Parse("#02cc9d")
+				//});
+				//s2.Items.Add(new ColumnItem {
+				//	Value = Tuesday,
+				//	Color = OxyColor.Parse("#02cc9d")
+				//});
+				//s2.Items.Add(new ColumnItem {
+				//	Value = Wednesday,
+				//	Color = OxyColor.Parse("#02cc9d")
+				//});
+				//s2.Items.Add(new ColumnItem {
+				//	Value = Thursday,
+				//	Color = OxyColor.Parse("#02cc9d")
+
+				//});
+				//s2.Items.Add(new ColumnItem {
+				//	Value = Friday,
+				//	Color = OxyColor.Parse("#02cc9d")
+				//});
+				//s2.Items.Add(new ColumnItem {
+				//	Value = Saturday,
+				//	Color = OxyColor.Parse("#02cc9d")
+				//});
+
+				model.Axes.Add(xaxis);
+				model.Axes.Add(yaxis);
+				model.Series.Add(s2);
+				model.PlotAreaBorderColor = OxyColors.Transparent;
+
+				return model;
+			}
+		}
+
+	}
+}
